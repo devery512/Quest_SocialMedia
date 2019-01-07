eef7e5727f8cde304c7aed4a8a38f1258bfa229b
Garrian BrownE <garrianbrown@gmail.com>
2019-01-07 00:51:31 -0600

    adding rails ap[ to repo.

---
+++ quest/app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :tweets
+  serialize :following, Array
+  serialize :followers, Array
+
+  mount_uploader :avatar, AvatarUploader
+
+  validates :username, presence: true, uniqueness: true
+end
+
+
